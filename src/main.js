a939e5ed567b1b084c997360bab8114b0f7fb3e8
Oleksii Patsiuk <patsyuck@ukr.net>
2021-03-28 22:00:23 +0300

    Додано збереження теми в localStorage

--- src/main.js
+++ src/main.js
@@ -5,15 +5,21 @@ const menuNode = document.querySelector('ul.menu')
 const markup = menuData.map(item => menuTemplate(item))
   .reduce((acc, value) => acc + value, '')
 const Theme = { LIGHT: 'light-theme', DARK: 'dark-theme' }
+const startTheme = localStorage.getItem('theme') || Theme.LIGHT
 const checkboxNode = document.querySelector('input.theme-switch__toggle')
 const bodyNode = document.querySelector('body')
 
 function switcher() {
   if (bodyNode.classList.contains(Theme.LIGHT)) {
     bodyNode.classList.replace(Theme.LIGHT, Theme.DARK)
+    localStorage.setItem('theme', Theme.DARK)
   } else {
     bodyNode.classList.replace(Theme.DARK, Theme.LIGHT)
-  }}
+    localStorage.setItem('theme', Theme.LIGHT)
+  }
+}
 
+bodyNode.classList.add(startTheme)
+if (startTheme === Theme.DARK) { checkboxNode.setAttribute('checked', true) }
 menuNode.insertAdjacentHTML('beforeend', markup)
 checkboxNode.addEventListener("change", switcher)
